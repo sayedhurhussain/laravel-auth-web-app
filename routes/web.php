706e3b9b262d6c2e386e3093be8d076ca0361b8f
Sayed Hur Hussain <67501891+sayedhurhussain@users.noreply.github.com>
2023-05-20 10:22:27 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 
-  
+ // Auth Route
 Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
